3f2434a37567b02df17412798dbdac464aa5e1b4
Muhammad Usman Amjad <muhammadusmanamjad1@users.noreply.github.com>
2021-02-19 15:47:10 +0500

    Change

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Restaurantly - v3.0.1
-* Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
